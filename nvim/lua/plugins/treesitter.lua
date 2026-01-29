fd4261a19e6cc1445a0ece10fdf8b5f5350ac120
zuice <jeff.allen127@gmail.com>
2026-01-28 16:33:19 -0800

    Code formatting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,22 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "typescript",
-      "tsx",
-      "javascript",
-      "lua",
-    },
-    highlight = {
-      enable = true,
-      disable = function(_, buf)
-          return vim.api.nvim_buf_get_name(buf):find("node_modules", 1, true) or vim.api.nvim_buf_line_count(buf) > 10000
-      end,
-    },
-    indent = { enable = true },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"typescript",
+			"tsx",
+			"javascript",
+			"lua",
+		},
+		highlight = {
+			enable = true,
+			disable = function(_, buf)
+				return vim.api.nvim_buf_get_name(buf):find("node_modules", 1, true)
+					or vim.api.nvim_buf_line_count(buf) > 10000
+			end,
+		},
+		indent = { enable = true },
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
-
